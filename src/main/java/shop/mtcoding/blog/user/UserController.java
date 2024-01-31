b9cb89481b254a6aa4cd3aa52af36660a27099e4
vosw1 <vosw1@naver.com>
2024-01-31 11:50:12 +0900

    록로êlogin error fix

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,78 +1,77 @@
 package shop.mtcoding.blog.user;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
-/**
+/*
  * 컨트롤러
- * 1. 요청받기(URI)
+ * 1. 요청받기 (URL - URI 포함 )
  * 2. http body는 어떻게? (DTO)
- * 3. 기본 mime 전략 : x-www.form.urlencoded (username=ssar&password=1234)
+ * 3. 기본 MIME 전략 : x-www-form-urlencoded (username=ssar&password=1234)
  * 4. 유효성 검사하기 (body 데이터가 있다면)
- * 5. 클라이언트가 View만 원하는지? 혹은 DB 처리 후 View도 원하는지?
- * 6. DB처리를 원하면 Model(DAO)에게 위임 후 view를 응답하면 끝
+ * 5. 클라이언트가 View(Mustache)만 원하는 지? 혹은  DB 처리 후 View도 원하는지???
+ * 6. View만 원하면 View를 응답하면 끈ㅌ
+ * 7. DB처리를 원하면 Model(DAO)에게 위임 후 view를 응답하면 끝
  */
-@RequiredArgsConstructor
+
+
 @Controller
 public class UserController {
-    private final UserRepository userRepository; //의존성을 주입해야하는 거는 다 final 붙여야함
+    private final UserRepository userRepository;
     private final HttpSession session;
 
-    //    public UserController(UserRepository userRepository, HttpSession session) { //IoC에 뜬걸 가져와서 쓴다 = 싱글톤으로 띄워서 쓰던거랑 같다
-//        this.userRepository = userRepository;
-//        this.session = session;
-//    }
+    public UserController(UserRepository userRepository, HttpSession session) {
+        this.userRepository = userRepository;
+        this.session = session;
+    }
+
     @PostMapping("/login")
-    public String login(UserRequest.loginDTO requestDTO){ //쿼리스트링으로 담아야하지만 예외로 로그인은 post요청함 원래는 get요청
+    public String login(UserRequest.loginDTO requestDTO) {
         //1. 유효성 검사
-        if(requestDTO.getUsername().length() <3){
+        if (requestDTO.getUsername().length() < 3) {
             return "error/400";
         }
 
-        //2. 모델 필요 select
-        User user=userRepository.findByUsernameAndPassword(requestDTO);
+        //2. 모델 필요 select * from user_tb where username= ? and password = ?
+        User user = userRepository.findByUsernameAndPassword(requestDTO);
 
-        if(user == null){
+        if (user == null) {
             return "error/401";
-        }else{
+        } else {
             session.setAttribute("sessionUser", user);
             return "redirect:/";
         }
 
-//        System.out.println(user);
-//        //3. 응답
-//        return "redirect:/"; //파일명 쓰지말기 또 페이지만들어있는데
+
+        //3. 응답
     }
 
-    @Transactional
-    @PostMapping ("/join")
-    public String join(UserRequest.joinDTO requestDTO){
-        System.out.println(requestDTO); //@DATA안에 String도 포함되어있음
-
+    @PostMapping("/join")
+    public String join(UserRequest.joinDTO requestDTO) {
+        System.out.println(requestDTO);
         //1. 유효성 검사
-        if(requestDTO.getUsername().length() <3){
+        if (requestDTO.getUsername().length() < 3) {
             return "error/400";
         }
-        //2. 동일  username 체크 -> 나중에 하나로 묶기
-User user = UserRepository.findByUsername(requestDTO.getUsername());
-        if(user == null) {
+        //2. 동일 username 체크 (나중에 하나의 트랜잭션으로 묶는 것이 좋다. )
+        User user = userRepository.findByUsername(requestDTO.getUsername());
+        if (user == null ){
             //3. Model에게 위임하기
             userRepository.save(requestDTO);
         } else {
             return "error/400";
         }
-        return "redirect:/loginForm"; //리다이렉션불러놓은게 있어서 다시부른거
+        return "redirect:/loginForm";
     }
 
     @GetMapping("/joinForm")
     public String joinForm() {
+
         return "user/joinForm";
     }
 
@@ -88,8 +87,7 @@ User user = UserRepository.findByUsername(requestDTO.getUsername());
 
     @GetMapping("/logout")
     public String logout() {
+        session.invalidate();
         return "redirect:/";
     }
-
-
 }
--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -1,58 +1,55 @@
 package shop.mtcoding.blog.user;
 
-import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.servlet.http.HttpSession;
 
-@Repository // 내가 new 하지 않아도 메모리에 띄울 수 있음
+@Repository
 public class UserRepository {
-    private EntityManager em; // 컴포지션
+    private EntityManager em;
 
-    public boolean UserRepository(String username) { // 생성자
-        Query query = em.createNativeQuery("select * from user_tb where username=?", User.class);
-        query.setParameter(1, username);
 
-        try {
-            User user = (User) query.getSingleResult();
-            return true; // 있음
-        } catch (Exception e) {
-            return false; // 없음
-        }
-
+    public UserRepository(EntityManager em) {
+        this.em = em;
     }
 
-    public static User findByUsername(String username) {
-
-    }
 
     @Transactional
-    public void save(UserRequest.joinDTO requestDTO) { // 컨트롤러는 정보를 전달하면서 때리고 위임함
+    public void save(UserRequest.joinDTO requestDTO) {
         Query query = em.createNativeQuery("insert into user_tb(username, password, email) values (?, ?, ?)");
         query.setParameter(1, requestDTO.getUsername());
         query.setParameter(2, requestDTO.getPassword());
         query.setParameter(3, requestDTO.getEmail());
-
         query.executeUpdate();
     }
 
     @Transactional
     public void saveV2(UserRequest.joinDTO requestDTO) {
-        User user = new User();// 통신을 통해 받은 데이터를 entity를 만들어서 담아보기
+        User user = new User();
         user.setUsername(requestDTO.getUsername());
         user.setPassword(requestDTO.getPassword());
         user.setEmail(requestDTO.getEmail());
-
         em.persist(user);
     }
-
+    @Transactional
     public User findByUsernameAndPassword(UserRequest.loginDTO requestDTO) {
         Query query = em.createNativeQuery("select * from user_tb where username=? and password=?", User.class);
         query.setParameter(1, requestDTO.getUsername());
         query.setParameter(2, requestDTO.getPassword());
+        try {
+            User user = (User) query.getSingleResult();
+            return user;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
+    public User findByUsername(String username) {
+        Query query = em.createNativeQuery("select * from user_tb where username=?", User.class);
+        query.setParameter(1, username);
         try {
             User user = (User) query.getSingleResult();
             return user;
