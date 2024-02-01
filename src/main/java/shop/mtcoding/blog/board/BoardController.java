fb3b30112a48b7e11ba565c2f30db5dd40b26b6d
vosw1 <vosw1@naver.com>
2024-02-01 16:13:04 +0900

    페이지 이동 설정

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -24,11 +24,31 @@ public class BoardController {
     private final BoardRepository boardRepository;
 
     // http://localhost:8080?page=0
-    @GetMapping({ "/", "/board" })
+    @GetMapping({ "/", "/board"})
     public String index(HttpServletRequest request, @RequestParam(defaultValue = "0") int page) {
-        System.out.println("페이지 : " +page);
-        List<Board> boardList = boardRepository.findAll();
-        request.setAttribute("boardList", boardList);
+        //System.out.println("페이지: " +page);
+        List<Board> boardList = boardRepository.findAll(page);
+        request.setAttribute("boardList", boardList); // 가방에 담음
+
+        int currentPage = page;
+        int nextPage = currentPage+1;
+        int prevPage = currentPage-1;
+        request.setAttribute("nextPage", nextPage);
+        request.setAttribute("prevPage", prevPage);
+        // 이것만 담으면 디세이브를 못한다.
+
+        // 현재 페이지가 퍼스트인지 라스트인지 만든다.
+        boolean first = currentPage == 0 ? true : false;
+
+        int boardTotalCount = boardRepository.findBoardTotalCount();
+        System.out.println(boardTotalCount);
+        int pagingCount = 3;
+        int totalPageCount = boardTotalCount/pagingCount;
+        boolean last = currentPage == totalPageCount? true : false;
+
+        request.setAttribute("first", first);
+        request.setAttribute("last", last);
+
         return "index";
     }
 
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -1,6 +1,7 @@
 package shop.mtcoding.blog.board;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -13,9 +14,21 @@ public class BoardRepository {
     private final EntityManager em; // jpa가 제공해줌
 
     // 조회니까 트랜잭션 필요없음
-    public List<Board> findAll() { // 보드 테이블의 모든 것 가지고 오기
-        Query query = em.createNativeQuery("select * from board_tb order by id desc limit 0,3", Board.class);
+    public List<Board> findAll(int page) { // 보드 테이블의 모든 것 가지고 오기
+        final int COUNT = 3;
+        int value = page*COUNT;
+        Query query = em.createNativeQuery("select * from board_tb order by id desc limit ?,?", Board.class);
+        query.setParameter(1, value);
+        query.setParameter(2, COUNT);
         List<Board> boardList = query.getResultList(); // 여러건
         return boardList;
     } // 이 결과를 리퀘스트에 담고 뷰 화면 가서 뿌리기
+
+    public int findBoardTotalCount() {
+        Query query = em.createNativeQuery("select count(*) from board_tb");
+
+        int boardTotalCount= ((Number)query.getSingleResult()).intValue();
+        //System.out.println("boardTotalCount : "+boardTotalCount);
+        return boardTotalCount;
+    }
 }
