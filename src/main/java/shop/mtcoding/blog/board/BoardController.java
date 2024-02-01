d0865e8fb7bfa64d5f42095fcb52bb33dea13c8e
vosw1 <vosw1@naver.com>
2024-02-01 16:46:16 +0900

    페이ì페이지 이동 리팩토링

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,16 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import shop.mtcoding.blog.user.User;
+import shop.mtcoding.blog._core.PagingUtil;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -38,13 +32,8 @@ public class BoardController {
         // 이것만 담으면 디세이브를 못한다.
 
         // 현재 페이지가 퍼스트인지 라스트인지 만든다.
-        boolean first = currentPage == 0 ? true : false;
-
-        int boardTotalCount = boardRepository.findBoardTotalCount();
-        System.out.println(boardTotalCount);
-        int pagingCount = 3;
-        int totalPageCount = boardTotalCount/pagingCount;
-        boolean last = currentPage == totalPageCount? true : false;
+        boolean first = PagingUtil.isFirst(currentPage);
+        boolean last = PagingUtil.isLast(currentPage, 4);
 
         request.setAttribute("first", first);
         request.setAttribute("last", last);
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -3,6 +3,7 @@ package shop.mtcoding.blog.board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.stereotype.Repository;
+import shop.mtcoding.blog._core.Constant;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
@@ -14,13 +15,13 @@ public class BoardRepository {
     private final EntityManager em; // jpa가 제공해줌
 
     // 조회니까 트랜잭션 필요없음
-    public List<Board> findAll(int page) { // 보드 테이블의 모든 것 가지고 오기
-        final int COUNT = 3;
-        int value = page*COUNT;
+    public List<Board> findAll(int page){
+        int value = page* Constant.PAGING_COUNT;
         Query query = em.createNativeQuery("select * from board_tb order by id desc limit ?,?", Board.class);
         query.setParameter(1, value);
-        query.setParameter(2, COUNT);
-        List<Board> boardList = query.getResultList(); // 여러건
+        query.setParameter(2, Constant.PAGING_COUNT);
+
+        List<Board> boardList = query.getResultList();
         return boardList;
     } // 이 결과를 리퀘스트에 담고 뷰 화면 가서 뿌리기
 
