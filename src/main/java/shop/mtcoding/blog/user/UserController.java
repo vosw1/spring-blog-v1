1e9875809e48022927cc78f71a6b6220c335fa12
vosw1 <you@example.com>
2024-02-05 16:33:36 +0900

    ì회원가입시 ì유효성 검사ì검사까지 구현

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.user;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,18 +8,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
+    private UserRepository userRepository; // null
+
+    public UserController(UserRepository userRepository) { //IOC 컨테이너에서 써치해서 찾아서 넣어줌
+        this.userRepository = userRepository;
+    } // 생성자를 만들어서 디폴트 생성자를 없애버림
+
     @PostMapping("/join")
-    public String join(UserRequest.JoinDTO requestDTO){
+    public String join(UserRequest.JoinDTO requestDTO) {
         System.out.println(requestDTO);
-        return "redirect:/loginForm";
+
+        // 1. 유효성 검사
+        if (requestDTO.getUsername().length() < 3) {
+            return "error/400";
+        }
+
+        userRepository.save(requestDTO); // 모델에 위임하기
+
+        return "redirect:/loginForm"; //리다이렉션불러놓은게 있어서 다시부른거
+
     }
 
-    @GetMapping("/joinForm")
+    @GetMapping("/joinForm") // view만 원함
     public String joinForm() {
         return "user/joinForm";
     }
 
-    @GetMapping("/loginForm")
+    @GetMapping("/loginForm") // view만 원함
     public String loginForm() {
         return "user/loginForm";
     }
