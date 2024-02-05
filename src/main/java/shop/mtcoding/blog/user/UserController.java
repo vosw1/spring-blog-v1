bf5607e029d0315c8fbe6d7d1a32d015efbd3823
vosw1 <you@example.com>
2024-02-05 16:46:44 +0900

    navbar change case

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.user;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
-    private UserRepository userRepository; // null
+    // fianl 변수는 반드시 초기화 되어야 함
+    private final UserRepository userRepository; // null
+    private final HttpSession session;
 
-    public UserController(UserRepository userRepository) { //IOC 컨테이너에서 써치해서 찾아서 넣어줌
+    public UserController(UserRepository userRepository, HttpSession session) {
         this.userRepository = userRepository;
-    } // 생성자를 만들어서 디폴트 생성자를 없애버림
+        this.session = session;
+    }
+
+    // 원래는 get요청이나 예외 post요청하면 됨
+    // 민감한 정보는 쿼리 스트링에 담아보낼 수 없음
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO requestDTO){
+
+        System.out.println(requestDTO); // toString -> @Data
+
+        if(requestDTO.getUsername().length() < 3){
+            return "error/400"; // ViewResolver 설정이 되어 있음. (앞 경로, 뒤 경로)
+        }
+
+        User user = userRepository.findByUsernameAndPassword(requestDTO);
+
+        if(user == null){ // 조회 안됨 (401)
+            return "error/401";
+        }else{ // 조회 됐음 (인증됨)
+            session.setAttribute("sessionUser", user); // 락카에 담음 (StateFul)
+        }
+
+        return "redirect:/"; // 컨트롤러가 존재하면 무조건 redirect 외우기
+    }
 
     @PostMapping("/join")
     public String join(UserRequest.JoinDTO requestDTO) {
@@ -22,11 +48,8 @@ public class UserController {
         if (requestDTO.getUsername().length() < 3) {
             return "error/400";
         }
-
         userRepository.save(requestDTO); // 모델에 위임하기
-
         return "redirect:/loginForm"; //리다이렉션불러놓은게 있어서 다시부른거
-
     }
 
     @GetMapping("/joinForm") // view만 원함
