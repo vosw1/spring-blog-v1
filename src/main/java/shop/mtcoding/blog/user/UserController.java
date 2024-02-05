8d7dd26090dee942e736b3528712a143ba39435b
vosw1 <you@example.com>
2024-02-05 15:54:42 +0900

    join->redirect check

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -2,9 +2,15 @@ package shop.mtcoding.blog.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @PostMapping("/join")
+    public String join(){
+        return "redirect:/loginForm";
+    }
 
     @GetMapping("/joinForm")
     public String joinForm() {
