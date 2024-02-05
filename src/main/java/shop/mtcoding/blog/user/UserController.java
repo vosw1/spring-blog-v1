f41bcd33f9c4dafa7553641bd47a8a07ff1d7360
vosw1 <you@example.com>
2024-02-05 17:30:55 +0900

    current loginer check gor test

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,11 +1,12 @@
 package shop.mtcoding.blog.user;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.context.annotation.ComponentScan;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@AllArgsConstructor
 @Controller
 public class UserController {
 
@@ -13,10 +14,11 @@ public class UserController {
     private final UserRepository userRepository; // null
     private final HttpSession session;
 
-    public UserController(UserRepository userRepository, HttpSession session) {
-        this.userRepository = userRepository;
-        this.session = session;
-    }
+    // @AllArgsConstructor를 사용하면서 필요 없어짐
+//    public UserController(UserRepository userRepository, HttpSession session) {
+//        this.userRepository = userRepository;
+//        this.session = session;
+//    }
 
     // 원래는 get요청이나 예외 post요청하면 됨
     // 민감한 정보는 쿼리 스트링에 담아보낼 수 없음
@@ -24,6 +26,7 @@ public class UserController {
     //민감한 정보는 쿼리 스트링에 담아보낼 수 없음
     @PostMapping("/login")
     public String login(UserRequest.LoginDTO requestDTO) {
+
         // 1. 유효성 검사
         if(requestDTO.getUsername().length() < 3) {
             return "error/400";
@@ -31,14 +34,12 @@ public class UserController {
 
         // 2. 모델 필요 select * from user_tb where username=? and password=?
         User user = userRepository.findByUsernameAndPassword(requestDTO); // DB에 조회할때 필요하니까 데이터를 받음
-
         if (user == null) {
             return "error/401";
         }else {
             session.setAttribute("sessionUser", user);
             return "redirect:/";
         }
-
     }
 
     @PostMapping("/join")
