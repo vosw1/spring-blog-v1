6fa529ea2deeb69c93654cb074b7f4d60ed746dc
vosw1 <you@example.com>
2024-02-05 15:52:22 +0900

    user_tb create2

--- src/main/java/shop/mtcoding/blog/user/User.java
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -5,18 +5,21 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-// user로 테이블명을 만들면, 키워드여서 안만들어질 수 있다. _tb 컨벤션 지키자.
-@Table(name="user_tb")
-@Data
-@Entity
-public class User {
-    @Id // PK 설정
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 전략
+@Data //GETTER, SETTER, toString 포함된 어노테이션
+@Entity // DB의 테이블과 매핑되는 클래스 임을 알려줌
+@Table(name = "user_tb") //테이블 이름 : user_tb
+public class User { // use_tb의 내용 DB에 담기
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private int id;
+
+    @Column(unique=true) // 유니크 설정
     private String username;
+
+    @Column(length = 60, nullable = false) // 길이 조정, 널 불가
     private String password;
     private String email;
 
-    // 카멜 표기법으로 만들면 DB는 created_at 으로 만들어진다. (언더스코어 기법)
-    private LocalDateTime createdAt;
+    // 자바 : 카멜 표기법 -> DB : _ (언더스코어 기법)
+    private LocalDateTime createdAt; // created_at
 }
